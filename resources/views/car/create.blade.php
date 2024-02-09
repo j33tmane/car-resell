e3f04c9e1d6f721687b927419dd5658b23831115
shriyak89 <shriyakulkarni89@gmail.com>
2024-02-09 12:54:50 +0530

    alter car db

--- resources/views/car/create.blade.php
+++ resources/views/car/create.blade.php
@@ -213,7 +213,7 @@
                                 <div class="mb-3">
                                     <label class="form-label">Vehical Number<small style="font-size:7px">(Without Space)</small></label>
                                     <div>
-                                        <input type="text" class="form-control" name="cno" maxlength="10" placeholder="MH09XX1234 (Without Space)" value="" required="">
+                                        <input type="text" class="form-control" name="car_number" maxlength="10" placeholder="MH09XX1234 (Without Space)" value="" required="">
                                     </div>
                                 </div>
                             </div>
@@ -290,39 +290,39 @@
                             <div class="col-md">
                               <h5>Features</h5>
                               <div class="form-check form-check-inline mt-3">
-                                <input class="form-check-input" type="checkbox" name=" featurs[]['abs]" value="1">
+                                <input class="form-check-input" type="checkbox" name=" features[]['abs]" value="1">
                                 <label class="form-check-label" for="inlineCheckbox1">ABS</label>
                               </div>
                               <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="featurs[]['clock']" value="2">
+                                <input class="form-check-input" type="checkbox" name="features[]" value="2">
                                 <label class="form-check-label" for="inlineCheckbox2">Central Locking</label>
                               </div>
                               <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="featurs[]['ac']" value="3">
+                                <input class="form-check-input" type="checkbox" name="features[]" value="3">
                                 <label class="form-check-label" for="inlineCheckbox2">Air Conditioner</label>
                               </div>
                               <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="featurs[]['pstrering']" value="4">
+                                <input class="form-check-input" type="checkbox" name="features[]" value="4">
                                 <label class="form-check-label" for="inlineCheckbox2">Power Steering</label>
                               </div>
                               <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="featurs[]['pwindow']" value="5">
+                                <input class="form-check-input" type="checkbox" name="features[]" value="5">
                                 <label class="form-check-label" for="inlineCheckbox2">Power Windows</label>
                               </div>
                               <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="featurs[]['heat']" value="6">
+                                <input class="form-check-input" type="checkbox" name="features[]" value="6">
                                 <label class="form-check-label" for="inlineCheckbox2">Auxiliary Heating</label>
                               </div>
                               <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="featurs[]['btooth']" value="7">
+                                <input class="form-check-input" type="checkbox" name="features[]" value="7">
                                 <label class="form-check-label" for="inlineCheckbox2">Bluetooth</label>
                               </div>
                               <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="featurs[]['cd']" value="8">
+                                <input class="form-check-input" type="checkbox" name="features[]" value="8">
                                 <label class="form-check-label" for="inlineCheckbox2">CD player</label>
                               </div>
                               <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="featurs[]['seat']" value="9">
+                                <input class="form-check-input" type="checkbox" name="features[]" value="9">
                                 <label class="form-check-label" for="inlineCheckbox2">Leather Seats</label>
                               </div>
                             </div> 
