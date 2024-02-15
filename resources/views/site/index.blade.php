8b9f74ac50464f07570aa4c8da3566ab389899cc
j33tmane <manjeetmane@gmail.com>
2024-02-15 11:57:06 +0530

    url bug fix

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -46,7 +46,7 @@
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top ">
         <div class="container d-flex align-items-center xs">
-            <h1 class="logo me-auto"><a href="index.html">ABHCars</a></h1>
+            <h1 class="logo me-auto"><a href="{{ url('/') }}">ABHCars</a></h1>
             <nav id="navbar" class="navbar">
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
